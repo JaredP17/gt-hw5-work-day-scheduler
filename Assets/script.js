f92ce21bbb9a3374eb83450e46097b875bdd5333
Jared Porter <jaredp17@me.com>
2020-06-17 02:32:17 -0400

    Dynamically generated the timeblocks and color coded for past, present, and future.

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,40 @@
 // VARIABLES
 var now = moment();
 var currentDay = now.format("dddd, MMMM Do");
-var currentHour = now.format("hA");
+var currentHour = now.hour();
 const businessHours = {
   start: 9, // 9 AM
   end: 17, // 5 PM
 };
+const timeStates = ["past", "present", "future"];
+var container = $(".container");
 
 // FUNCTIONS DEFINTIONS
 function populateTimeBlocks() {
   for (var i = businessHours.start; i <= businessHours.end; i++) {
-    
-    console.log(moment(i, "H").format("hA"));
+    var hour = moment(i, "H").format("hA");
+    var state = "";
+    if (i < currentHour) {
+      state = timeStates[0];
+    } else if (i === currentHour) {
+      state = timeStates[1];
+    } else {
+      state = timeStates[2];
+    }
+
+    container.append(
+      '<div class="row timeblock">' +
+        '<div class="col-1 hour">' +
+        hour +
+        "</div>" +
+        '<textarea class="col-10 description ' +
+        state +
+        '" id="' +
+        i +
+        '"></textarea>' +
+        '<button class="col-1 btn saveBtn"><i class="fas fa-save"></i></button>' +
+        "</div>"
+    );
   }
 }
 
@@ -24,6 +47,6 @@ populateTimeBlocks();
 // LOGS
 console.log(now);
 console.log(currentDay);
-console.log(currentHour);
+console.log(typeof currentHour);
 var m = moment(9, "H").format("hA");
 console.log(m);
